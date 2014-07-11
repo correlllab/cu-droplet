d00b426566d4365bfebed44ea69035feca9222e9
kailaashr <kailaashr@gmail.com>
2014-07-11 17:58:19 -0600

    Python Code updated to send required lines of data during programming mode

--- FloorProgramming/MySerialWrapper.py
+++ FloorProgramming/MySerialWrapper.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 import serial
 import time
+baud = 4800
+
 def open_serial_port(port):
     """ Function open_serial_port(port): Opens the serial port supplied as input (e.g. 'COM9') 
     and tests to make sure it's working. WARNING: The caller of this function is responsible for 
@@ -27,13 +29,26 @@ def set_global_port(port_h):
 def cleanup():
     global_port.close()
 
+def check_chars():
+    if(global_port.inWaiting()>0):
+        global_port.flushInput()
+        return True
+    else:
+        return False
+
 def read():
-    while global_port.inWaiting() > 0:
-        dat = global_port.readline()
-        print('\t\t', end='')
-        print(dat.strip())
+    dat = global_port.read()
+    global_port.flushInput()
+    return dat
+    #while global_port.inWaiting() > 0:
+    #dat = global_port.readline()
+    #return dat
+        #print('\t\t', end='')
+        #print(dat.strip())
 
 def write(data):
-    out_dat = data + '\r\n';
-    global_port.write(out_dat)
-    global_port.flushOutput()
+    #out_dat = data + '\r\n';
+    global_port.write(data)
+    global_port.flush()
+    while(global_port.outWaiting()>0):
+        1+1 #do nothing

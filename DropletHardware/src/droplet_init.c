284ab7cbf9d17bc85cc4c34f353f49f9b2df9ad2
John Klingner <john.klingner@colorado.edu>
2014-09-28 20:30:04 -0600

    Working on the calculate_droplet_id() function, since we have a collision.

--- DropletHardware/src/droplet_init.c
+++ DropletHardware/src/droplet_init.c
@@ -8,10 +8,11 @@ void init_all_systems()
 {
 	Config32MHzClock();
 	
-	calculate_id_number();
+
 	
 	scheduler_init();			if(INIT_DEBUG_MODE) printf("SCHEDULER INIT\r\n"); //This will probably never print, since you need pc_com for printf to happen, but pc_com needs the scheduler.
 	pc_com_init();				if(INIT_DEBUG_MODE) printf("PC COM INIT\r\n");
+	calculate_id_number();	
 	RGB_LED_init();				if(INIT_DEBUG_MODE) printf("LED INIT\r\n");	
 	rgb_sensor_init();			if(INIT_DEBUG_MODE) printf("RGB SENSE INIT\r\n");
 	power_init();				if(INIT_DEBUG_MODE) printf("POWER INIT\r\n");
@@ -32,22 +33,27 @@ void calculate_id_number()
 	if(INIT_DEBUG_MODE >= 1)	printf("get id number\r\n");
 
 	uint32_t pgm_bytes = 0;
-	uint32_t crc = 0;
+	uint16_t crc = 0;
 	
 	// calculate local id
 	// simply crc of user sig row
 	// likely to be unique and constant for each chip
 	NVM_CMD = NVM_CMD_READ_CALIB_ROW_gc;
+	
+	uint8_t l_byte, h_byte;
 
-	for (uint32_t i = 0x08; i <= 0x15; i++)			
+	for (uint32_t i = 0x00; i <= 0x15; i++)			
 	{
 		pgm_bytes = pgm_read_word_far(i);
-		crc = _crc16_update(crc, pgm_bytes);
+		printf("0x%02X: ",i);
+		printf("%02X\r\n", pgm_bytes&0xFF);
+		crc = _crc16_update(crc, (uint16_t)pgm_bytes);
 	}
 
 	NVM_CMD = NVM_CMD_NO_OPERATION_gc;
 
 	droplet_ID = crc;
+	printf("ID: %04hX\r\n",droplet_ID);
 }
 
 void enable_interrupts()

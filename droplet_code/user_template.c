9976eb348ce210f51d7883ffda5b0792ba813412
John Klingner <john.klingner@colorado.edu>
2015-09-23 13:24:32 -0600

    Uncommenting user_template.c

--- droplet_code/user_template.c
+++ droplet_code/user_template.c
@@ -1,52 +1,46 @@
-//#include "user_template.h"
-//
+#include "user_template.h"
+
+
+/*
+ * Any code in this function will be run once, when the robot starts.
+ */
+void init()
+{
+
+}
+
+/*
+ * The code in this function will be called repeatedly, as fast as it can execute.
+ */
+void loop()
+{
+
+}
+
+/*
+ * After each pass through loop(), the robot checks for all messages it has 
+ * received, and calls this function once for each message.
+ */
+void handle_msg(ir_msg* msg_struct)
+{
+
+}
 
 ///*
- //* Any code in this function will be run once, when the robot starts.
+ //*	The function below is optional - commenting it in can be useful for debugging if you want to query
+ //*	user variables over a serial connection.
  //*/
-//void init()
+//uint8_t user_handle_command(char* command_word, char* command_args)
 //{
-//
+	//return 0;
 //}
-//
+
 ///*
- //* The code in this function will be called repeatedly, as fast as it can execute.
- //*/
-//void loop()
+ //*	The function below is optional - if it is commented in, and the leg interrupts have been turned on
+ //*	with enable_leg_status_interrupt(), this function will get called when that interrupt triggers.
+ //*/	
+//void	user_leg_status_interrupt()
 //{
-	//uint32_t time_floor = (get_time()/300);
-	//if(((time_floor+6*getFixedIndex(get_droplet_id()))%50)==0){
-		//set_rgb(30,0,35);
-		//broadcast_rnb_data();
-	//}else{
-		//delay_ms(300);	
-	//}
+	//
 //}
-//
-///*
- //* After each pass through loop(), the robot checks for all messages it has 
- //* received, and calls this function once for each message.
- //*/
-//void handle_msg(ir_msg* msg_struct)
-//{
-//
-//}
-//
-/////*
- ////*	The function below is optional - commenting it in can be useful for debugging if you want to query
- ////*	user variables over a serial connection.
- ////*/
-////uint8_t user_handle_command(char* command_word, char* command_args)
-////{
-	////return 0;
-////}
-//
-/////*
- ////*	The function below is optional - if it is commented in, and the leg interrupts have been turned on
- ////*	with enable_leg_status_interrupt(), this function will get called when that interrupt triggers.
- ////*/	
-////void	user_leg_status_interrupt()
-////{
-	////
-////}
-//
+

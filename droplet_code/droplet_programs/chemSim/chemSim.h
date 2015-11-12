2b7ae1d8d7e3a6a41734e8aeeaf4ebbc35cfdd5f
John Klingner <audrey.randall@colorado.edu>
2015-11-12 10:26:17 -0700

    Code has been cleaned up significantly.

--- droplet_code/droplet_programs/chemSim/chemSim.h
+++ droplet_code/droplet_programs/chemSim/chemSim.h
@@ -43,7 +43,6 @@ typedef struct
 	float heading;
 	uint8_t last_msg_t; //time that this atom last sent a message to me
 	uint8_t bonded;
-	uint8_t stability;
 }Near_Atom;
 
 typedef struct 
@@ -57,12 +56,6 @@ typedef struct
 	char flag_array[9];
 	int8_t orbitals[6]; //-1 for empty orbital, 0 for orbital that the recipient doesn't occupy, 1 for an orbital it does occupy.
 }Bond_Made_Msg;
-
-typedef struct  
-{
-	uint16_t ID;
-	uint8_t stability;
-}Stability_Tool;
 
 typedef struct{
 	int16_t x;
@@ -83,7 +76,7 @@ FixedRNBMeas fixedRNBMeasurements[NUM_FIXED_DROPLETS];
 
 Near_Atom near_atoms[12]; //this number is pretty arbitrary.
 Atom NULL_ATOM = {{0,0,0,0,0,0,0,0},{0,0,0,0,0,0},{'0','0'},0,0,0};
-Near_Atom NULL_NEAR_ATOM = {{{0,0,0,0,0,0,0,0},{0,0,0,0,0,0},{'0','0'},0,0,0}, 0, 0, 0, 0, 0, 0};
+Near_Atom NULL_NEAR_ATOM = {{{0,0,0,0,0,0,0,0},{0,0,0,0,0,0},{'0','0'},0,0,0}, 0, 0, 0, 0, 0};
 volatile uint32_t bondDelay;
 volatile uint16_t potentialPartner;
 volatile uint32_t tap_delay;
@@ -93,13 +86,11 @@ volatile uint32_t last_rnb;
 uint32_t last_chem_ID_broadcast;
 volatile uint16_t global_blink_timer;
 uint16_t my_molecule[15];  //this differs from bonded_atoms in that it includes all atoms in the molecule, not just ones directly bonded to self. 
-Stability_Tool my_molecule_stability[15];
 uint16_t used_in_stability[15];
 uint8_t my_molecule_length;
 uint8_t collided;
 float target_spot;
 uint16_t target_id;
-uint8_t stability;
 uint32_t timeLastMoved;
 int16_t deltaGself;		//my molecule's deltaG
 int16_t deltaGself_p;	//my molecule's deltaG if the atom I'm talking to were to bond with me
@@ -116,21 +107,15 @@ void handle_msg(ir_msg* msg_struct);
 void user_leg_status_interrupt();
 
 uint8_t addAtomToMolecule(uint16_t atom_id);
-int16_t addAtomToStability(uint16_t ID, int16_t cur_s, uint8_t cur_s_size);
 void addToBondedAtoms(uint16_t ID, uint8_t index, uint8_t num_bonds);
-//void add_to_my_orbitals(uint16_t ID, uint8_t num_bonds);
 uint8_t addToNearAtoms();
-//void broadcastChemID(Atom ID);
+void breakAndFormBonds(Atom* near_atom_ptr, uint16_t senderID);
 void breakBond(Atom* near_atom, uint16_t sender_ID);
-uint8_t calculateMyStability();
 void calculatePath(float target, uint16_t range, float bearing);
 void calculateTarget(Atom* nearAtom, uint16_t range, float bearing, float heading);
 uint8_t cleanOtherMolecule(Atom* near_atom, uint8_t* dirtyMolecule, uint8_t* cleanMolecule, uint8_t count);
 int comparison(uint8_t* aPtr, uint8_t* bPtr);
-uint8_t convertBearingToEmitorDir(float bearing);
-void convertEmitorDirToArray(uint8_t dirs, uint8_t* bits);
 void createStateMessage(State_Msg* msg, char flag);
-void detectOtherDroplets();
 void formBond(uint16_t senderID, Atom* near_atom, char flag);
 void foundBondRoutine(char flag);
 uint8_t getAtomicNumFromID(uint16_t ID);
@@ -139,47 +124,30 @@ Atom* getAtomFromAtomicNum(uint8_t atomicNum);
 Atom* getAtomFromID(uint16_t ID);
 float getChiFromID(uint16_t ID);
 uint8_t getFilledOrbs();
-//uint8_t getFixedIndex(uint16_t id);
-//void getOrbitals(Atom* atom);
+void getNameFromAtomicNum(char* name, uint8_t atomicNum);
+void getOtherDeltaGs(uint8_t *mc, uint8_t mc_length, int16_t* deltaGother, int16_t* deltaGother_p, int16_t* deltaGother_m);
+char IMRTest(Atom* near_atom, int16_t deltaGother, int16_t deltaGother_p, int16_t deltaGother_m, uint16_t senderID);
 void initAtomState();
-void initRandomMove(uint16_t direc);
-uint8_t isGoodRNB(float rng, float bearing, uint16_t ID);
+void initBondedAtoms(Atom atom);
 void makePossibleBonds(Atom* near_atom_ptr, char flag, int16_t deltaGother, int16_t deltaGother_p, int16_t deltaGother_m, uint16_t senderID);
-void matchMolecule(uint16_t* other_molecule, uint8_t length, uint16_t exclude_id);
 void modifyValencesIonic(char* newValence, Atom* near_atom_ptr, uint16_t senderID);
 void modifyValencesCovalent(char* newValence, Atom* near_atom_ptr, uint16_t senderID);
 void moveToTarget(uint16_t rng, float bearing);
 void msgBondedAtoms(Atom* near_atom, uint16_t new_blink, uint16_t sender_ID);
-void msgBondMade(ir_msg* msg_struct, char flag);
-//void msgContactFirst(uint16_t senderID);
-//void msgContactSecond(char* msg, uint16_t senderID);
-//void msgOrbital(uint16_t* other_bonded_atoms, uint16_t senderID);
 void msgPossibleBond(ir_msg* msg_struct);
 void msgState(ir_msg* msg_struct);
-char IMRTest(Atom* near_atom, int16_t deltaGother, int16_t deltaGother_p, int16_t deltaGother_m, uint16_t senderID);
-uint8_t* orbitalOrder(uint8_t *valence);
 void packValences(uint8_t* packed_shells, int8_t* shells);
 void printBondedAtoms();
+void printMolecularReactions(uint8_t* otherMol);
 void printValence(int8_t valence[]);
 void removeAtomFromMolecule(uint16_t atom_id);
-void repairBondedAtoms();
-void repairValence();
+void returnLightToDefault();
+void sendTargetedMsg(char* msg, uint8_t msgLength, uint16_t target);
 void setAtomColor(Atom* ID);
-void transmitMoleculeStruct(uint16_t exclude_id, char flag);
 void unpackValences(uint8_t* packed_shells, int8_t* shells);
 void updateDeltaGs();
-void updateMolecule(uint16_t* atNums, uint8_t length, uint16_t sender);
-void updateMoleculeTwo(Atom* near_atom, uint16_t senderID);
+void updateMolecule(Atom* near_atom, uint16_t senderID);
 uint8_t updateNearAtoms(Atom* near_atom, ir_msg* msg_struct);
-//void updatePositionEstimate();
-void updateStability();
-uint8_t valenceState();
-
-
-/*
- int8_t bear = (int8_t)ceilf((3.0*last_good_rnb.bearing)/M_PI);
- uint8_t newDir = ((6-bear)%6);
-
-*/
+void updateTargetId();
 
 #endif

6a6b9d5d58611ce0b0f67277f09e1d7f3666e018
Praveen Gnanasekaran <praveengnan94@gmail.com>
2017-03-17 13:03:47 -0600

    edge following implemented without using range and bearing- works only for close distances

--- droplet_code/user_template.c
+++ droplet_code/user_template.c
@@ -6,6 +6,9 @@ int8_t receiving_motor_settings_from_another_droplet_flag=0;
 int8_t sign_bit;
 int16_t motor_settings_received_count=0;
 int16_t motor_signs_count=0;
+
+uint8_t max_value_dir;
+int16_t max_ir_value;
 /*
  * Any code in this function will be run once, when the robot starts.
  */
@@ -19,17 +22,19 @@ void init(){
 
 void loop(){
 
-	if(get_time()-last_rnb_broadcast > 3000){
-		broadcast_rnb_data();
+	if(get_time()-last_rnb_broadcast > 1000){
+		//check_collisions();
+		//printf("\n\r");
+		//broadcast_rnb_data();
 		last_rnb_broadcast=get_time();
 	}
 	
-move_to_closest();
+	follow_edge(0);
 
 	if(rnb_updated){
 		//printf("RANGE %d Bearing %d Heading %d\n\r",last_good_rnb.range,last_good_rnb.bearing,last_good_rnb.heading);
 		//edge_following(last_good_rnb.range,last_good_rnb.bearing);
-		if(last_good_rnb.bearing)
+		
 		rnb_updated = 0; //Note! This line must be included for things to work properly.
 	}
 
@@ -179,37 +184,52 @@ void send_motor_settings(){
 	ulti_count=0;
 	set_rgb(0,0,0);
 }
+void check_ir_coll_max(uint8_t* input_dir, int16_t* input_val){
+		int16_t measured_vals[6];
+		uint8_t dirs=0;
+		if(!ir_is_available(ALL_DIRS)){
+			printf_P(PSTR("IR Hardware busy, probably sending a message? Can't check collisions.\r\n"));
+			return;
+		}
+		for(uint8_t i=0;i<6;i++) ir_rxtx[i].status = IR_STATUS_BUSY_bm;
+		uint16_t curr_power = get_all_ir_powers();
+		set_all_ir_powers(256);
+		
+		for(uint8_t i=0;i<6;i++) ir_led_on(i);
+		busy_delay_us(250);
+		get_ir_sensors(measured_vals, 5);
+		int16_t max_ir_value=-1000;
+		uint8_t max_value_dir;
+		for(uint8_t i=0;i<6;i++){
+			//printf("%4d ", measured_vals[i]);
+			if(measured_vals[i]>max_ir_value){
+				max_ir_value=measured_vals[i];
+				max_value_dir=i;
+				//printf("MAX %4d  DIR %d\n\r", max_ir_value,max_value_dir);
+			}
+		}
+		//printf("\r\n");
+		delay_ms(500);
+		for(uint8_t i=0;i<6;i++) ir_led_off(i);
+		set_all_ir_powers(curr_power);
+		for(uint8_t i=0;i<6;i++) ir_rxtx[i].status = 0;
+		*input_dir= max_value_dir;
+		*input_val= max_ir_value;
+}
 void move_to_closest(){
-	int16_t measured_vals[6];
-	uint8_t dirs=0;
-	if(!ir_is_available(ALL_DIRS)){
-		printf_P(PSTR("IR Hardware busy, probably sending a message? Can't check collisions.\r\n"));
-		return;
-	}
-	for(uint8_t i=0;i<6;i++) ir_rxtx[i].status = IR_STATUS_BUSY_bm;
-	uint16_t curr_power = get_all_ir_powers();
-	set_all_ir_powers(256);
-	
-	for(uint8_t i=0;i<6;i++) ir_led_on(i);
-	busy_delay_us(250);
-	get_ir_sensors(measured_vals, 5);
-	int16_t max_ir_value=0;
-	uint8_t max_value_dir;
-	for(uint8_t i=0;i<6;i++){
-		 printf("%4d ", measured_vals[i]);
-		 if(measured_vals[i]>max_ir_value){
-		 max_ir_value=measured_vals[i];
-		 max_value_dir=i;
-			printf("MAX %4d  DIR %d\n\r", max_ir_value,max_value_dir);
-		 }
-	}
-	printf("\r\n");
-	for(uint8_t i=0;i<6;i++) ir_led_off(i);
-	set_all_ir_powers(curr_power);
-	for(uint8_t i=0;i<6;i++) ir_rxtx[i].status = 0;		
+	//if dir==0 , move_left
+	//if dir==1 , move_right
+		
+	check_ir_coll_max(max_value_dir,max_ir_value);
 	if(max_ir_value>=1000){
 	if((max_value_dir==0)||(max_value_dir==5)){
-	move_steps(0,10);
+		schedule_task(5,broadcast_rnb_data,NULL);
+		//if(rnb_updated){
+		//printf("RANGE %d Bearing %d Heading %d\n\r",last_good_rnb.range,last_good_rnb.bearing,last_good_rnb.heading);
+		//if(last_good_rnb.range<=2)
+			move_steps(0,10);
+			rnb_updated=0;
+		//}
 	//while(is_moving());
 	}
 	else if((max_value_dir==1)||(max_value_dir==2)){
@@ -219,10 +239,54 @@ void move_to_closest(){
 	else
 	move_steps(7,10);
 	}
-	
-	
-	
 }
+
+void follow_edge(int dir)
+{
+	//dir=0 ==> move in left direction using IR pair 1
+	//dir=1 ==> move in right direction using IR pair 4
+	
+	if(dir==0)
+	{
+		//check max ir coll dir
+		check_ir_coll_max(&max_value_dir,&max_ir_value);
+		if(max_value_dir==1)
+		{
+			move_steps(0,10);
+		}
+		else if((max_value_dir==0)||(max_value_dir==5)){// if 0 or 5 , move anticlockwise
+			move_steps(7,5);
+		}
+		else {
+			move_steps(6,5);
+		}
+		//while(!is_moving());
+		delay_ms(400);
+	}
+	else if(dir==1)
+	{
+		check_ir_coll_max(&max_value_dir,&max_ir_value);
+		if(max_value_dir==4)
+		{
+			move_steps(0,10);
+		}
+		else if((max_value_dir==0)||(max_value_dir==5)){// if 0 or 5 , move clockwise
+			move_steps(6,5);
+		}
+		else {
+			move_steps(7,5);
+		}
+		//while(!is_moving());
+		delay_ms(400);
+		
+	}
+	printf("max dir:%d max value:%d\n\r",max_value_dir,max_ir_value);
+		//check if dir 1 has max IR -> if yes, move fwd 10 steps
+//									-> if no, move_anticlockwise 10 steps and check again
+
+
+}
+
 void check_if_motor_calibrated()
 {
 	int cnt=0;
@@ -239,7 +303,6 @@ void check_if_motor_calibrated()
 	else
 	printf("Calibrated\n\r");
 }
-
 /*
  *	The function below is optional - commenting it in can be useful for debugging if you want to query
  *	user variables over a serial connection. It should return '1' if command_word was a valid command,

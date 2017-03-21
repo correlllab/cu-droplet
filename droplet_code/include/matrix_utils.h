24d7492311cc8dea9c12346a8d1c7dc82e1feebb
john-klingner <john.klingner@colorado.edu>
2017-03-21 17:45:02 -0600

    Master branch needed matrix_utils

---
+++ droplet_code/include/matrix_utils.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <droplet_init.h>
+#include <math.h>
+
+typedef float Vector[3];
+typedef float Matrix[3][3];
+
+void vectorAdd(Vector* dst, Vector* a, Vector* b);		//dst = a + b
+void vectorSubtract(Vector* dst, Vector* a, Vector* b);	//dst = a - b
+
+void matrixScale(Matrix* A, float s);
+void matrixTimesVector(Vector* dst, Matrix* A, Vector* b);	//dst = A x b
+
+void matrixAdd(Matrix* DST, Matrix* A, Matrix* B);			//DST = A + B
+void matrixSubtract(Matrix* DST, Matrix* A, Matrix* B);		//DST = A - B
+void matrixMultiply(Matrix* DST, Matrix* A, Matrix* B);		//DST = A x B
+void matrixInplaceMultiply(Matrix* DST, Matrix* A, Matrix* B);
+
+void matrixTranspose(Matrix* DST, Matrix* A);
+void matrixInplaceTranspose(Matrix* A);
+
+uint8_t positiveDefiniteQ(Matrix* A);
+
+void matrixInverse(Matrix* DST, Matrix* A);					//DST = A^(-1)
+void matrixInplaceInverse(Matrix* A);
+float matrixDet(Matrix* A);
+float matrixTrace(Matrix* A);
+
+void matrixCopy(Matrix* DST, Matrix* A);						//DST = A
+void printMatrix(Matrix* A);
+void printMatrixMathematica(Matrix* A);

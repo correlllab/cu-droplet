1bdb9bfc83fdbc6fa225593f1412a46f0bc89e96
john-klingner <john.klingner@colorado.edu>
2017-07-07 15:23:40 -0600

    Code done I think? Basically just positions.c

--- droplet_code/user_template.h
+++ droplet_code/user_template.h
@@ -2,6 +2,29 @@
 
 #include "droplet_init.h"
 
-void		init(void);
-void		loop(void);
+//#define RNB_DEBUG_MODE
+#ifdef RNB_DEBUG_MODE
+#define RNB_DEBUG_PRINT(format, ...) printf_P(PSTR(format), ##__VA_ARGS__)
+#else
+#define RNB_DEBUG_PRINT(format, ...)
+#endif
+
+#define SLOT_LENGTH_MS			397
+#define SLOTS_PER_FRAME			38
+#define FRAME_LENGTH_MS			(((uint32_t)SLOT_LENGTH_MS)*((uint32_t)SLOTS_PER_FRAME))
+#define LOOP_DELAY_MS			17
+
+uint32_t	frameCount;
+uint32_t	frameStart;
+uint16_t	mySlot;
+uint16_t	loopID;
+
+void		init();
+void		loop();
 void		handle_msg(ir_msg* msg_struct);
+
+
+static inline uint16_t getSlot(id_t id){
+	return (id%(SLOTS_PER_FRAME-1));
+}
+

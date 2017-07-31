cf63e030f873f20c9a3188114668a1ff818d8b86
john-klingner <john.klingner@colorado.edu>
2017-07-31 16:58:47 -0600

    Initial commit.

--- droplet_code/user_template.c
+++ droplet_code/user_template.c
@@ -1,30 +1,93 @@
 #include "user_template.h"
 
 /*
- * any code in this function will be run once, when the robot starts.
+ * Any code in this function will be run once, when the robot starts.
  */
 void init(){
-
+	#ifndef SYNCHRONIZED
+	printf("Error! It doesn't make sense to do TDMA without synchronizing the Droplets!\r\n");
+	#endif
+	loopID = 0xFFFF;
+	frameCount = 0;
+	mySlot = (get_droplet_id()%(SLOTS_PER_FRAME-1));
+	frameStart = get_time();
+	acksSincePing=0;
 }
 
 /*
- * the code in this function will be called repeatedly, as fast as it can execute.
+ * The code in this function will be called repeatedly, as fast as it can execute.
  */
 void loop(){
+	uint32_t frameTime = get_time()-frameStart;
+	if(frameTime>FRAME_LENGTH_MS){
+		frameTime = frameTime - FRAME_LENGTH_MS;
+		frameStart += FRAME_LENGTH_MS;
+		frameCount++;
+	}
+	if(loopID!=(frameTime/SLOT_LENGTH_MS)){
+		loopID = frameTime/SLOT_LENGTH_MS;
+		if(loopID==mySlot){
+			sendPingMsg();
+		}else if(loopID==(SLOTS_PER_FRAME-1)){
+			//compute new state based on messages.
+		}
+		
+	}
+	if(rnb_updated){
+		//Handle rnb data in last_good_rnb struct.
+	}
+	delay_ms(LOOP_DELAY_MS);
+}
 
+
+void sendPingMsg(){
+	PingMsg msg;
+	msg.flag = PING_MSG_FLAG;
+	for(uint8_t i=0;i<FILLER_LENGTH;i++){
+		msg.filler[i] = 0xAB;
+	}
+	ir_send(ALL_DIRS, (char*)(&msg), sizeof(PingMsg));
+	printf("Acks Since Last Ping: %hu\r\n", acksSincePing);
+	acksSincePing=0;
+}
+
+void handlePingMsg(PingMsg* msg, id_t src){
+	sendAckMsg(src);
+}
+
+void sendAckMsg(id_t src){
+	AckMsg msg;
+	msg.flag = ACK_MSG_FLAG;
+	ir_targeted_send(ALL_DIRS, (char*)(&msg), sizeof(AckMsg), src);
+}
+
+void handleAckMsg(AckMsg* msg){
+	if(msg->src == get_droplet_id()){
+		acksSincePing++;
+	}
 }
 
 /*
- * after each pass through loop(), the robot checks for all messages it has 
+ * After each pass through loop(), the robot checks for all messages it has 
  * received, and calls this function once for each message.
  */
 void handle_msg(ir_msg* msg_struct){
-
+	if(msg_struct->length == sizeof(PingMsg)){
+		PingMsg* msg = (PingMsg*)(msg_struct->msg);
+		if(msg->flag == PING_MSG_FLAG){
+			handlePingMsg(msg, msg_struct->sender_ID);
+		}
+	}else if(msg_struct->length == sizeof(AckMsg)){
+	AckMsg* msg = (AckMsg*)(msg_struct->msg);
+	if(msg->flag == ACK_MSG_FLAG){
+		handleAckMsg(msg);
+	}
+}
 }
 
 ///*
- //*	the function below is optional - commenting it in can be useful for debugging if you want to query
- //*	user variables over a serial connection. it should return '1' if command_word was a valid command,
+ //*	The function below is optional - commenting it in can be useful for debugging if you want to query
+ //*	user variables over a serial connection. It should return '1' if command_word was a valid command,
  //*  '0' otherwise.
  //*/
 //uint8_t user_handle_command(char* command_word, char* command_args){
--- droplet_code/user_template.h
+++ droplet_code/user_template.h
@@ -2,6 +2,48 @@
 
 #include "droplet_init.h"
 
+#define PING_MSG_FLAG 'P'
+#define ACK_MSG_FLAG  'A'
+
+/*
+ * SLOT_LENGTH_MS should be long enough for all of the communicating
+ * and rnb_broadcasting the Droplets need to do in their slots.
+ */
+#define SLOT_LENGTH_MS			331
+/*
+ * This should be big enough for your density of robots that
+ * the probability of two adjacent robots having the same slot
+ * is very low.
+ */
+#define SLOTS_PER_FRAME			21 
+#define FRAME_LENGTH_MS			(((uint32_t)SLOT_LENGTH_MS)*((uint32_t)SLOTS_PER_FRAME))
+#define LOOP_DELAY_MS			10 //This number should stay small, but above 0.
+
+#define FILLER_LENGTH 1
+
+uint32_t	frameCount;
+uint32_t	frameStart;
+uint16_t	mySlot;
+uint8_t	acksSincePing;
+uint16_t	loopID;
+
+typedef struct ping_message_struct{
+	char flag;
+	uint8_t filler[FILLER_LENGTH];
+}PingMsg;
+
+typedef struct ack_message_struct{
+	char flag;
+	id_t src;
+}AckMsg;
+
 void		init(void);
 void		loop(void);
-void		handle_msg(ir_msg* msg_struct);
+
+void		sendPingMsg(void);
+void		handlePingMsg(PingMsg* msg, id_t src);
+
+void		sendAckMsg(id_t src);
+void		handleAckMsg(AckMsg* msg);
+
+void		handle_msg			(ir_msg* msg_struct);

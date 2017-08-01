420fb4eaa9a9345b82a59c0beadd2d578de4e4d5
john-klingner <john.klingner@colorado.edu>
2017-08-01 15:00:29 -0600

    Working pretty well. I switched things over to a CSMA approach for both ping and ack and it made a huge difference.

--- droplet_code/user_template.c
+++ droplet_code/user_template.c
@@ -4,67 +4,30 @@
  * Any code in this function will be run once, when the robot starts.
  */
 void init(){
-	#ifndef SYNCHRONIZED
-	printf("Error! It doesn't make sense to do TDMA without synchronizing the Droplets!\r\n");
-	#endif
-	loopID = 0xFFFF;
-	frameCount = 0;
-	mySlot = (get_droplet_id()%(SLOTS_PER_FRAME-1));
-	frameStart = get_time();
 	acksSincePing=0;
+	pingMsgPending = 0;
+	lastPingMsgSent = get_time() + (rand_short()>>1); //uniformly random number between 0 and ~32 seconds.
 }
 
 /*
  * The code in this function will be called repeatedly, as fast as it can execute.
  */
 void loop(){
-	uint32_t frameTime = get_time()-frameStart;
-	if(frameTime>FRAME_LENGTH_MS){
-		frameTime = frameTime - FRAME_LENGTH_MS;
-		frameStart += FRAME_LENGTH_MS;
-		frameCount++;
-	}
-	if(loopID!=(frameTime/SLOT_LENGTH_MS)){
-		loopID = frameTime/SLOT_LENGTH_MS;
-		if(loopID==mySlot){
-			sendPingMsg();
-		}else if(loopID==(SLOTS_PER_FRAME-1)){
-			//compute new state based on messages.
+	if(!pingMsgPending){
+		if((get_time()-lastPingMsgSent) > PING_MSG_PERIOD){
+			pingMsgPending = 1;
+			prepPingMsg();
 		}
-		
-	}
-	if(rnb_updated){
-		//Handle rnb data in last_good_rnb struct.
 	}
 	delay_ms(LOOP_DELAY_MS);
 }
 
-
-void sendPingMsg(){
-	PingMsg msg;
-	msg.flag = PING_MSG_FLAG;
-	for(uint8_t i=0;i<FILLER_LENGTH;i++){
-		msg.filler[i] = 0xAB;
-	}
-	ir_send(ALL_DIRS, (char*)(&msg), sizeof(PingMsg));
-	printf("Acks Since Last Ping: %hu\r\n", acksSincePing);
-	acksSincePing=0;
-}
-
 void handlePingMsg(PingMsg* msg, id_t src){
-	sendAckMsg(src);
-}
-
-void sendAckMsg(id_t src){
-	AckMsg msg;
-	msg.flag = ACK_MSG_FLAG;
-	ir_targeted_send(ALL_DIRS, (char*)(&msg), sizeof(AckMsg), src);
+	prepAckMsg(src);
 }
 
 void handleAckMsg(AckMsg* msg){
-	if(msg->src == get_droplet_id()){
-		acksSincePing++;
-	}
+	acksSincePing++;
 }
 
 /*
@@ -77,19 +40,85 @@ void handle_msg(ir_msg* msg_struct){
 		if(msg->flag == PING_MSG_FLAG){
 			handlePingMsg(msg, msg_struct->sender_ID);
 		}
-	}else if(msg_struct->length == sizeof(AckMsg)){
-	AckMsg* msg = (AckMsg*)(msg_struct->msg);
-	if(msg->flag == ACK_MSG_FLAG){
-		handleAckMsg(msg);
+	}
+	if(msg_struct->length == sizeof(AckMsg)){
+		AckMsg* msg = (AckMsg*)(msg_struct->msg);
+		if(msg->flag == ACK_MSG_FLAG){
+			handleAckMsg(msg);
+		}
 	}
 }
+
+uint32_t getBackoffTime(uint8_t N){
+	uint8_t randMax = (1<<N) - 1;
+	return (uint32_t)(rand_real()*randMax*16);
 }
 
-///*
- //*	The function below is optional - commenting it in can be useful for debugging if you want to query
- //*	user variables over a serial connection. It should return '1' if command_word was a valid command,
- //*  '0' otherwise.
- //*/
-//uint8_t user_handle_command(char* command_word, char* command_args){
-	//return 0;
-//}
+void prepPingMsg(){
+	PingMsgNode* node = (PingMsgNode*)myMalloc(sizeof(PingMsgNode));
+	node->numTries = 0;
+	for(uint8_t i=0;i<9;i++){
+		node->msg.filler[i] = rand_norm(0,1);
+	}
+	node->msg.flag = PING_MSG_FLAG;
+	sendPingMsg(node);
+}
+
+void sendPingMsg(PingMsgNode* node){
+	if(ir_is_busy(ALL_DIRS)){
+		(node->numTries)++;
+		if(node->numTries>7){
+			//printf("Giving up on ping.\r\n");
+			myFree(node);
+			ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
+				lastPingMsgSent=get_time();
+				pingMsgPending=0;
+			}
+		}else{
+			schedule_task(getBackoffTime(node->numTries), (arg_func_t)sendPingMsg, node);
+		}
+	}else{
+		ir_send(ALL_DIRS, (char*)(&(node->msg)), sizeof(PingMsg));
+		//printf("Ping successful after %hu tries.\r\n", node->numTries);
+		uint8_t numAcksReceived;
+		ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
+			numAcksReceived = acksSincePing;
+			pingMsgPending = 0;
+			lastPingMsgSent=get_time();
+			acksSincePing=0;
+		}
+		printf("Acks Since Last Ping: %hu\r\n", numAcksReceived);
+		myFree(node);
+	}
+}
+
+
+//Takes all of the information needed for a botMeasMsg and packs it up in to a BotMeasMsgNode,
+//then calls sendBotMeasMsg with it.
+void prepAckMsg(id_t id){
+	AckMsgNode* node = (AckMsgNode*)myMalloc(sizeof(AckMsgNode));
+	node->numTries = 0;
+	node->tgt = id;
+	for(uint8_t i=0;i<9;i++){
+		node->msg.filler[i] = rand_norm(0,1);
+	}
+	node->msg.flag = ACK_MSG_FLAG;
+	sendAckMsg(node);
+}
+
+//Sends an AckMsg using a poor man's CSMA protocol.
+void sendAckMsg(AckMsgNode* node){
+	if(ir_is_busy(ALL_DIRS)){
+		(node->numTries)++;
+		if(node->numTries>7){
+			//printf("Giving up on ack for %04X.\r\n", node->tgt);
+			myFree(node);
+		}else{
+			schedule_task(getBackoffTime(node->numTries), (arg_func_t)sendAckMsg, node);
+		}
+	}else{
+		ir_targeted_send(ALL_DIRS, (char*)(&(node->msg)), sizeof(AckMsg), node->tgt);
+		//printf("Ack for %04X successful after %hu tries.\r\n", node->tgt, node->numTries);
+		myFree(node);
+	}
+}
--- droplet_code/user_template.h
+++ droplet_code/user_template.h
@@ -5,45 +5,45 @@
 #define PING_MSG_FLAG 'P'
 #define ACK_MSG_FLAG  'A'
 
-/*
- * SLOT_LENGTH_MS should be long enough for all of the communicating
- * and rnb_broadcasting the Droplets need to do in their slots.
- */
-#define SLOT_LENGTH_MS			331
-/*
- * This should be big enough for your density of robots that
- * the probability of two adjacent robots having the same slot
- * is very low.
- */
-#define SLOTS_PER_FRAME			21 
-#define FRAME_LENGTH_MS			(((uint32_t)SLOT_LENGTH_MS)*((uint32_t)SLOTS_PER_FRAME))
-#define LOOP_DELAY_MS			10 //This number should stay small, but above 0.
+#define PING_MSG_PERIOD 32768
 
-#define FILLER_LENGTH 1
+#define LOOP_DELAY_MS			17 //This number should stay small, but above 0.
 
-uint32_t	frameCount;
-uint32_t	frameStart;
-uint16_t	mySlot;
 uint8_t	acksSincePing;
-uint16_t	loopID;
+uint32_t lastPingMsgSent;
+uint8_t pingMsgPending;
 
 typedef struct ping_message_struct{
 	char flag;
-	uint8_t filler[FILLER_LENGTH];
+	float filler[9];
 }PingMsg;
+
+typedef struct ping_msg_node_struct{
+	PingMsg msg;
+	uint8_t numTries;
+}PingMsgNode;
 
 typedef struct ack_message_struct{
 	char flag;
-	id_t src;
+	float filler[9];
 }AckMsg;
+
+typedef struct ack_msg_node_struct{
+	AckMsg msg;
+	id_t tgt;
+	uint8_t numTries;
+}AckMsgNode;
 
 void		init(void);
 void		loop(void);
 
-void		sendPingMsg(void);
 void		handlePingMsg(PingMsg* msg, id_t src);
-
-void		sendAckMsg(id_t src);
 void		handleAckMsg(AckMsg* msg);
 
-void		handle_msg			(ir_msg* msg_struct);
+void		handle_msg(ir_msg* msg_struct);
+
+uint32_t	getBackoffTime(uint8_t N);
+void		prepPingMsg(void);
+void		sendPingMsg(PingMsgNode* node);
+void		prepAckMsg(id_t id);
+void		sendAckMsg(AckMsgNode* node);
